2b51ba25c9af6f873f89250d1263be467f5080b0
alyssajohnasen <72017694+alyssajohnasen@users.noreply.github.com>
2021-08-04 22:38:31 -1000

    Add ScrolllReveale options to index.html & about.html

--- src/app.js
+++ src/app.js
@@ -1,8 +1,73 @@
-ScrollReveal({ reset: true }).reveal('.recent-work', { delay: 500 });
-ScrollReveal().reveal('.projects' , { delay: 1000 });
+ScrollReveal().reveal('.recent-work', {
+    delay: 300,
+    duration: 1000,
+    easing: 'ease-in',
+    reset: true,
+    distance: '50px',
+    origin: 'top'
+});
+ScrollReveal().reveal('.dictionary-photo' , {
+    delay: 500,
+    duration: 1000,
+    easing: 'ease-in',
+    reset: true,
+    distance: '50px',
+    origin: 'top',
+});
+ScrollReveal().reveal('.react-weather-photo' , { 
+    delay: 1200,
+    duration: 1000,
+    easing: 'ease-in',
+    reset: true,
+    distance: '50px',
+    origin: 'top' 
+});
+ScrollReveal().reveal('.vanilla-js-weather-photo' , { 
+    delay: 1300,
+    duration: 1000,
+    easing: 'ease-in',
+    reset: true,
+    distance: '50px',
+    origin: 'top'  
+  });
 
-ScrollReveal({ reset: true }).reveal('.name', { delay: 500 });
-ScrollReveal({ reset: true }).reveal('.tagline' , { delay: 1000 });
-ScrollReveal({ reset: true }).reveal('.about-me-info' , { delay: 1500 });
-ScrollReveal({ reset: true }).reveal('.view-my-work' , { delay: 1500 });
-ScrollReveal({ reset: true }).reveal('.about-me-image', { delay: 1800 });
+ScrollReveal().reveal('.name', { 
+    delay: 300,
+    duration: 1000,
+    easing: 'ease-in',
+    reset: true,
+    distance: '50px',
+    origin: 'top' 
+});
+ScrollReveal().reveal('.tagline' , { 
+    delay: 500,
+    duration: 1000,
+    easing: 'ease-in',
+    reset: true,
+    distance: '50px',
+    origin: 'top'
+ });
+ScrollReveal().reveal('.about-me-info' , { 
+    delay: 1200,
+    duration: 1000,
+    easing: 'ease-in',
+    reset: true,
+    distance: '50px',
+    origin: 'top'
+ });
+ScrollReveal().reveal('.view-my-work' , { 
+    delay: 1300,
+    duration: 1000,
+    easing: 'ease-in',
+    reset: true,
+    distance: '50px',
+    origin: 'top' 
+ });
+ScrollReveal().reveal('.about-me-image', { 
+    delay: 1500,
+    duration: 1000,
+    easing: 'ease-in',
+    reset: true,
+    distance: '50px',
+    origin: 'top' 
+ });
