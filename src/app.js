0afb7a19fb7f93454bb2a5107045a3cbd3f238bc
alyssajohnasen <72017694+alyssajohnasen@users.noreply.github.com>
2021-08-04 20:21:26 -1000

    Added ScrollRevealjs feature to index.html & about.html

---
+++ src/app.js
@@ -0,0 +1,8 @@
+ScrollReveal({ reset: true }).reveal('.recent-work', { delay: 500 });
+ScrollReveal().reveal('.projects' , { delay: 1000 });
+
+ScrollReveal({ reset: true }).reveal('.name', { delay: 500 });
+ScrollReveal({ reset: true }).reveal('.tagline' , { delay: 1000 });
+ScrollReveal({ reset: true }).reveal('.about-me-info' , { delay: 1500 });
+ScrollReveal({ reset: true }).reveal('.view-my-work' , { delay: 1500 });
+ScrollReveal({ reset: true }).reveal('.about-me-image', { delay: 1800 });
